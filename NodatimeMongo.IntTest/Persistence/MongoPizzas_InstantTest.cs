bc139441c81d180e817acacd016baaedb7f4dee0
Richard Gross <richard.gross@maibornwolff.de>
2021-06-12 17:58:01 +0200

    t: add test for finding documents by instant

--- NodatimeMongo.IntTest/Persistence/MongoPizzas_InstantTest.cs
+++ NodatimeMongo.IntTest/Persistence/MongoPizzas_InstantTest.cs
@@ -1,8 +1,11 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using DotNet.Testcontainers.Containers.Builders;
 using DotNet.Testcontainers.Containers.Modules;
 using FluentAssertions;
 using MongoDB.Driver;
+using NodaTime;
 using Richargh.Sandbox.NodatimeMongo.Domain;
 using Richargh.Sandbox.NodatimeMongo.Persistence;
 using Xunit;
@@ -46,6 +49,32 @@ namespace Richargh.Sandbox.NodatimeMongo.IntTest.Persistence
             var result = await _testling.FindById(pizza.Id);
             result!.InstantUtc.ToUnixTimeMilliseconds().Should().Be(pizza.InstantUtc.ToUnixTimeMilliseconds());
             // result!.InstantUtc.Should().Be(pizza.InstantUtc); // cannot assert this because some nanosecond precision is lost after deserialization
+        }
+        
+        [Fact(DisplayName = "Should be able to find one Pizza older than one minute ago")]
+        public async Task OnePizzaOlder()
+        {
+            // given
+            var pizza = InstantPizza.Now(InstantPizzaId.Random());
+            await _testling.Put(pizza);
+            // when
+            var result = await _testling.FindOlderThan(
+                Instant.FromDateTimeUtc(DateTime.UtcNow) - Duration.FromMinutes(1));
+            // then
+            result.Select(x => x.Id).Should().HaveCount(1).And.Contain(pizza.Id);
+        }
+        
+        [Fact(DisplayName = "Should not be able to find one Pizza older than the future")]
+        public async Task NoPizzaOlderthanFuture()
+        {
+            // given
+            var pizza = InstantPizza.Now(InstantPizzaId.Random());
+            await _testling.Put(pizza);
+            // when
+            var result = await _testling.FindOlderThan(
+                Instant.FromDateTimeUtc(DateTime.UtcNow) + Duration.FromMinutes(1));
+            // then
+            result.Select(x => x.Id).Should().HaveCount(0);
         }
     }
 }
